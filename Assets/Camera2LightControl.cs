e0df9da9d26f31811b29fe39920a5550a67d88dd
Florwie <114574121+Florwie@users.noreply.github.com>
2023-08-10 16:51:38 +0200

    Added Camera 2 Light Control

---
+++ Assets/Camera2LightControl.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using System.Threading;
+using Opc.Ua;
+using Opc.Ua.Client;
+using Opc.Ua.Configuration;
+using System.Linq;
+using UtilityFunctions;
+
+public class Camera2LightControl : MonoBehaviour
+{
+    private OPCUA_Client OPCUA_Client;
+    private GameObject robotGameObject;
+    private List <ComponentClasses.LightComponent> lightComponents = new List<ComponentClasses.LightComponent>();
+
+    bool checkForStart()
+    {
+        bool start = true;
+        if(lightComponents.Any(item => OPCUA_Client.allNodes["Camera2"].childrenNodes[item.name].result.Value == null))
+        {
+            start = false;
+        }
+        return start;
+    }
+
+    void updateLights()
+    {
+        foreach(ComponentClasses.LightComponent lightComponent in lightComponents)
+            {
+                List <bool> currentState = new List<bool>();
+                float[] currentColor = null; 
+
+                int stateReading = (int) OPCUA_Client.allNodes["Camera2"].childrenNodes[lightComponent.name].result.Value;
+                currentState.Add(stateReading != 0);
+                lightComponent.UpdateValues(currentState, currentColor);
+            }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        robotGameObject = GameObject.Find("pm_robot");
+        OPCUA_Client = robotGameObject.GetComponent<OPCUA_Client>();
+        List<GameObject> childrenGameObjects = GenericFunctions.getChildrenGameObjects(gameObject); 
+        foreach(GameObject childGameObject in childrenGameObjects)
+        {
+            lightComponents.Add(new ComponentClasses.LightComponent(childGameObject));
+        }  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(checkForStart())
+        {
+            updateLights();
+        }
+    }
+}
