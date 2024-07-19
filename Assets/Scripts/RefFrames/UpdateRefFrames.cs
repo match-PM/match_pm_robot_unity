b78ccc6257780c0acb403537d26f71881fc0ee2a
Florwie <wiemann@match.uni-hannover.de>
2024-07-19 15:57:44 +0200

    services added

---
+++ Assets/Scripts/RefFrames/UpdateRefFrames.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ROS2
+{
+public class UpdateRefFrames : MonoBehaviour
+{
+
+    private ROS2UnityComponent ros2Unity;
+    private ROS2Node ros2Node;
+    private ISubscription<tf2_msgs.msg.TFMessage> chatter_sub;
+    private tf2_msgs.msg.TFMessage recievedRequest;
+
+    bool done = false;  // has everything worked?
+
+    List<string> alreadyAdded = new List<string>(); // to evade multiples RefFrames at TFs
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // open a Subscriber at /tf topic
+        ros2Unity = GetComponent<ROS2UnityComponent>();    
+        
+        if (ros2Node == null && ros2Unity.Ok())
+        {
+            ros2Node = ros2Unity.CreateNode("ROS2UnityListenerRefFrames");
+            chatter_sub = ros2Node.CreateSubscription<tf2_msgs.msg.TFMessage>("/tf", msg => newData(msg)/*, msg => drawNewRefFrames(msg)*/);
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!done)
+        {
+            drawNewRefFrames(recievedRequest);
+        }
+    }
+
+    // callback
+    // is called, when /tf-data has been recieved
+    void newData(tf2_msgs.msg.TFMessage incData)
+    {
+        // save, because "Find" in drawNewRefFrames can only be run on mainThread
+        recievedRequest = incData;
+        try
+        {
+            // remove subscription, since we only need one message
+            ros2Node.RemoveSubscription<tf2_msgs.msg.TFMessage>(chatter_sub);
+        }
+        catch(Exception ex)
+        {
+            Debug.Log("Exception in newData: " + ex.Message);
+        }
+    }
+    
+    // Instatiates the refFrame Prefab at the given tf-Frame
+    void drawNewRefFrames(tf2_msgs.msg.TFMessage incomming)
+    {        
+        done = true; // in case the first Message was broken
+        
+        try{
+            geometry_msgs.msg.TransformStamped[] transforms = incomming.Transforms;
+          
+            foreach (geometry_msgs.msg.TransformStamped ts in transforms)
+            {            
+                if (!alreadyAdded.Contains(ts.Header.Frame_id))
+                {
+                    GameObject go = GameObject.Find(ts.Header.Frame_id); // Find TF
+                    
+                    Instantiate(Resources.Load<GameObject>("Prefabs/RefFrame"), go.transform);  //Append refFrame
+                    alreadyAdded.Add(ts.Header.Frame_id);                                
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            Debug.Log("Exception in drawNewRefFrames: " + ex.Message + "; Trying again");
+            // get next message and try again
+            done = false;
+            chatter_sub = ros2Node.CreateSubscription<tf2_msgs.msg.TFMessage>("/tf", msg => newData(msg));
+        }
+    }
+
+}
+}
---
+++ Assets/Scripts/SpawnerService/SpawnSmallerObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ROS2;
+
+public class SpawnSmallerObject : MonoBehaviour
+{
+    public Mesh meshPart;
+  
+    public MeshFilter mf;
+    public MeshRenderer mr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        // Add Material from Resources folder to MeshRenderer
+        mr = GetComponent<MeshRenderer>();
+        var newMat = Resources.Load<Material>("Materials/green");
+        mr.material = newMat;
+        
+        // Add mesh to MeshFilter
+        mf = GetComponent<MeshFilter>();
+        mf.mesh = meshPart;
+    }
+}
